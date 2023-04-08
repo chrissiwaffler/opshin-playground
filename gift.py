e9a779cc20d66a6dbfe1aee3f389922fcd58beab
Christoph Waffler <waffler.christoph@gmail.com>
2023-04-09 01:58:03 +0200

    push test

---
+++ gift.py
@@ -0,0 +1,13 @@
+from opshin.prelude import *
+
+@dataclass()
+class CancelDatum(PlutusData):
+    pubkeyhash: bytes
+
+
+def validator(datum: CancelDatum, redeemer: None, context: ScriptContext) -> None:
+    sig_present = False
+    for s in context.tx_info.signatories:
+        if datum.pubkeyhash == s:
+            sig_present = True
+    assert sig_present
